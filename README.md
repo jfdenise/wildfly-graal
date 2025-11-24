00a5bead652e50dcb54e12060909bcd4b7bd6472
Jean-Francois Denise <jfdenise@gmail.com>
2025-11-24 15:26:26 +0100

    Minimal WildFly core server starting

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # wildfly-graal
 
+# WildFly core
+
+# Build dependencies
+
+* galleon install wildfly-core --layers=base-server,-git-history --dir=min-server2
+* clone and build:  https://github.com/jfdenise/jboss-modules/pull/new/2.x-graal-poc
+* cd module-launcher
+* mvn clean install
+
+## Prepare the built image
+
+* Run : JAVA_OPTS="-agentlib:native-image-agent=config-output-dir=./min-server-graal-agent2,experimental-class-define-support=true" sh ./min-server2/bin/standalone.sh
+* Kill the server
+
+## Build the image
+
+In this script replace some jdenise with your context.
+
+* run: sh ./build-wildfly-image.sh
+
+## Run the image
+
+* ./ModuleLauncher-1.0-SNAPSHOT
+
+
+# Simple examples:
+
 * Build api, provider, hello and module-launcher
 * cp hello/target/HelloModule-1.0-SNAPSHOT.jar modules/hello/main/hello.jar
 * cp provider/target/ProviderModule-1.0-SNAPSHOT.jar modules/provider/main/provider.jar
@@ -29,3 +56,19 @@ native-image -jar module-launcher/target/ModuleLauncher-1.0-SNAPSHOT.jar -cp /Us
 ### Run the image
 
 ./launcher.Launcher
+
+# Attempt to remove classes from the classpath and add discovered ones
+
+* Run the application with options to track defined classes:
+
+java -agentlib:native-image-agent=config-output-dir=./graal-agent,experimental-class-define-support=true  -cp /Users/jdenise/.m2/repository/org/jboss/modules/jboss-modules/2.2.0.Final/jboss-modules-2.2.0.Final.jar:module-launcher/target/ModuleLauncher-1.0-SNAPSHOT.jar launcher.Launcher
+
+* Build the image provising the dumped configuration and no classes in the 
+
+native-image -jar module-launcher/target/ModuleLauncher-1.0-SNAPSHOT.jar -cp /Users/jdenise/.m2/repository/org/jboss/modules/jboss-modules/2.2.0.Final/jboss-modules-2.2.0.Final.jar --initialize-at-build-time launcher.Launcher -H:ConfigurationFileDirectories=graal-agent  
+
+## Wildfly core
+
+JAVA_OPTS="-agentlib:native-image-agent=config-output-dir=./min-server-graal-agent2,experimental-class-define-support=true" sh ./min-server2/bin/standalone.sh
+
+native-image -jar module-launcher/target/ModuleLauncher-1.0-SNAPSHOT.jar -cp /Users/jdenise/.m2/repository/org/jboss/modules/jboss-modules/2.3.0.Final-SNAPSHOT/jboss-modules-2.3.0.Final-SNAPSHOT.jar --initialize-at-build-time=launcher.Launcher,org.jboss.modules -H:ConfigurationFileDirectories=min-server-graal-agent   --enable-url-protocols=jar
--- module-launcher/src/main/java/launcher/Launcher.java
+++ module-launcher/src/main/java/launcher/Launcher.java
@@ -1,13 +1,23 @@
 package launcher;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.jboss.modules.LocalModuleLoader;
 import org.jboss.modules.Module;
-import org.jboss.modules.ModuleLoadException;
 import org.jboss.modules.ModuleLoader;
+import org.jboss.modules.log.StreamModuleLogger;
 
 public class Launcher {
 
@@ -15,19 +25,97 @@ public class Launcher {
     private static final String SYSPROP_KEY_MODULE_PATH = "module.path";
     private static final String SYSPROP_KEY_SYSTEM_MODULES = "jboss.modules.system.pkgs";
     private static Module helloModule;
+    private static List<Module> modules = new ArrayList<>();
+
     static {
-        System.out.println("CALLEDx");
-        LocalModuleLoader loader = (LocalModuleLoader) setupModuleLoader(Paths.get("modules").toAbsolutePath().toString());
-        System.out.println("LOCAL LOADER " + loader);
+        Path modulesDir = Paths.get("min-server2/modules").toAbsolutePath();
+        LocalModuleLoader loader = (LocalModuleLoader) setupModuleLoader(modulesDir.toString());
+        System.out.println("LOCAL LOADER + DEBUG ENABLED" + loader);
+        Module.setModuleLogger(new StreamModuleLogger(System.out));
         try {
-            helloModule = loader.loadModule("hello");
-        } catch (ModuleLoadException ex) {
+            // Force loading of this class that must be initialized
+            Class.forName("org.wildfly.common._private.CommonMessages");
+            Map<String, Path> all = new HashMap<>();
+            handleModules(modulesDir, all);
+            for (String k : all.keySet()) {
+                try {
+                    System.out.println("Load module " + k);
+                    Module mod = loader.loadModule(k);
+                    if (k.equals("org.jboss.as.standalone")) {
+                        helloModule = mod;
+                    }
+                    modules.add(mod);
+                } catch (Exception ex) {
+                    System.out.println("EX " + ex);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
             System.out.println("EX " + ex);
         }
     }
+
+    static void handleModules(Path modulesDir, Map<String, Path> moduleXmlByPkgName) throws IOException {
+        final Path layersDir = modulesDir.resolve("system").resolve("layers");
+        try (Stream<Path> layers = Files.list(layersDir)) {
+            final Iterator<Path> i = layers.iterator();
+            while (i.hasNext()) {
+                final Path layerDir = i.next();
+                findModules(layerDir, moduleXmlByPkgName);
+                if (moduleXmlByPkgName.isEmpty()) {
+                    throw new IOException("Modules not found in " + layerDir);
+                }
+            }
+        }
+    }
+
+    static void findModules(Path modulesDir, Map<String, Path> moduleXmlByPkgName) throws IOException {
+        Files.walkFileTree(modulesDir, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                final Path moduleXml = dir.resolve("module.xml");
+                if (!Files.exists(moduleXml)) {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                String packageName;
+                if (moduleXml.getParent().getFileName().toString().equals("main")) {
+                    packageName = modulesDir.relativize(moduleXml.getParent().getParent()).toString();
+                } else {
+                    packageName = modulesDir.relativize(moduleXml.getParent()).toString();
+                }
+                packageName = packageName.replace(File.separatorChar, '.');
+                moduleXmlByPkgName.put(packageName, moduleXml);
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.TERMINATE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
     public static void main(String[] args) throws Exception {
-        
+        System.setProperty("org.jboss.boot.log.file",Paths.get("min-server2/standalone/log/server.log").toAbsolutePath().toString());
+        Path p = Paths.get("min-server2/standalone/standalone/logging.properties");
+        System.setProperty("logging.configuration",p.toUri().toString());
+        System.setProperty("jboss.home.dir", Paths.get("min-server2").toAbsolutePath().toString());
+        System.setProperty("user.home", Paths.get("/users/jdenise").toAbsolutePath().toString());
+        System.setProperty("java.home", Paths.get("/tmp/java").toAbsolutePath().toString());
+        System.setProperty("jboss.server.base.dir", Paths.get("min-server2/standalone").toAbsolutePath().toString());
         helloModule.run(args);
+
     }
 
     private static String trimPathToModulesDir(String modulePath) {
