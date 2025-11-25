91ac125907140183041fd42b0b682206edb200b2
Jean-Francois Denise <jfdenise@gmail.com>
2025-11-25 11:56:55 +0100

    At runtime, logmanager at build time, no rotating file

--- module-launcher/src/main/java/launcher/Launcher.java
+++ module-launcher/src/main/java/launcher/Launcher.java
@@ -2,6 +2,8 @@ package launcher;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
@@ -13,41 +15,58 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.jboss.modules.LocalModuleLoader;
+import org.jboss.modules.ModularContentHandlerFactory;
+import org.jboss.modules.ModularURLStreamHandlerProvider;
 import org.jboss.modules.Module;
 import org.jboss.modules.ModuleLoader;
+import org.jboss.modules.log.StreamModuleLogger;
 
 public class Launcher {
 
     private static final String SYSPROP_KEY_CLASS_PATH = "java.class.path";
     private static final String SYSPROP_KEY_MODULE_PATH = "module.path";
     private static final String SYSPROP_KEY_SYSTEM_MODULES = "jboss.modules.system.pkgs";
-    private static Module helloModule;
-    public static Map<String, Module> modules = new HashMap<>();
-    public static Map<String, Module> callerModules= new HashMap<>();
+    //private static Module helloModule;
+    //public static Map<String, Module> modules = new HashMap<>();
+    //public static Map<String, Module> callerModules= new HashMap<>();
     static {
         //Module.setModuleLogger(new StreamModuleLogger(System.out));
         try {
         // Force loading of this class that must be initialized
          //   Class.forName("org.wildfly.common._private.CommonMessages");
-        Path modulesDir = Paths.get("min-server2/modules").toAbsolutePath();
-        LocalModuleLoader loader = (LocalModuleLoader) setupModuleLoader(modulesDir.toString());
-        System.out.println("LOCAL LOADER + DEBUG ENABLED" + loader);
-
-            
-            Map<String, Path> all = new HashMap<>();
-            handleModules(modulesDir, all);
-            for (String k : all.keySet()) {
-                try {
-                    System.out.println("Load module " + k);
-                    Module mod = loader.loadModule(k);
-                    if (k.equals("org.jboss.as.standalone")) {
-                        helloModule = mod;
-                    }
-                    modules.put(k, mod);
-                } catch (Exception ex) {
-                    System.out.println("EX " + ex);
+//        Path modulesDir = Paths.get("modules").toAbsolutePath();
+//        LocalModuleLoader loader = (LocalModuleLoader) setupModuleLoader(modulesDir.toString());
+//        System.out.println("LOCAL LOADER + DEBUG ENABLED" + loader);
+//        helloModule = loader.loadModule("hello");
+//
+//            
+//            Map<String, Path> all = new HashMap<>();
+//            handleModules(modulesDir, all);
+//            for (String k : all.keySet()) {
+//                try {
+//                    System.out.println("Load module " + k);
+//                    Module mod = loader.loadModule(k);
+//                    if (k.equals("org.jboss.as.standalone")) {
+//                        helloModule = mod;
+//                    }
+//                    modules.put(k, mod);
+//                } catch (Exception ex) {
+//                    System.out.println("EX " + ex);
+//                }
+//            }
+try {
+                    System.out.println("REGISTER DATA PROTOCOL");
+                    URL.setURLStreamHandlerFactory(ModularURLStreamHandlerProvider.INSTANCE);
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                    System.out.println("ERROR WHEN REGISTERING");
+                    // todo log a warning or something
                 }
-            }
+                try {
+                    URLConnection.setContentHandlerFactory(ModularContentHandlerFactory.INSTANCE);
+                } catch (Throwable t) {
+                    // todo log a warning or something
+                }
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("EX " + ex);
@@ -109,13 +128,35 @@ public class Launcher {
         System.setProperty("org.jboss.boot.log.file",Paths.get("min-server2/standalone/log/server.log").toAbsolutePath().toString());
         Path p = Paths.get("min-server2/standalone/configuration/logging.properties");
         System.out.println("Setting java.util.logging.manager=org.jboss.logmanager.LogManager");
-        System.setProperty("java.util.logging.manager","org.jboss.logmanager.LogManager");
-        System.setProperty("logging.configuration",p.toUri().toString());
+        //System.setProperty("java.util.logging.manager","org.jboss.logmanager.LogManager");
+        //System.setProperty("logging.configuration",p.toUri().toString());
         System.setProperty("jboss.home.dir", Paths.get("min-server2").toAbsolutePath().toString());
         System.setProperty("user.home", Paths.get("/users/jdenise").toAbsolutePath().toString());
         System.setProperty("java.home", Paths.get("/tmp/java").toAbsolutePath().toString());
         System.setProperty("jboss.server.base.dir", Paths.get("min-server2/standalone").toAbsolutePath().toString());
-        
+//Module.setModuleLogger(new StreamModuleLogger(System.out));
+
+        Path modulesDir = Paths.get("min-server2/modules").toAbsolutePath();
+        LocalModuleLoader loader = (LocalModuleLoader) setupModuleLoader(modulesDir.toString());
+        Module helloModule = null;
+        Map<String, Path> all = new HashMap<>();
+            handleModules(modulesDir, all);
+            for (String k : all.keySet()) {
+                try {
+                    
+                    //Module mod = loader.loadModule(k);
+                    if (k.equals("org.jboss.as.standalone")) {
+                        System.out.println("Load module " + k);
+                        Module mod = loader.loadModule(k);
+                        helloModule = mod;
+                    }
+                    //modules.put(k, mod);
+                } catch (Exception ex) {
+                    System.out.println("EX " + ex);
+                }
+            }
+        System.out.println("LOCAL LOADER + DEBUG ENABLED" + loader);
+        //Module helloModule = loader.loadModule("hello");
         helloModule.run(args);
 
     }
